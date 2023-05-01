216f2820c79728aef7bdd28af74b89c68dbe13bf
ewerton-pontini <ewerton.pontini@picpay.com>
2023-05-01 11:08:52 -0300

    Update Application.kt

--- src/main/kotlin/com/systems/pontini/Application.kt
+++ src/main/kotlin/com/systems/pontini/Application.kt
@@ -5,12 +5,15 @@ import com.systems.pontini.config.configureDatabase
 import com.systems.pontini.config.configureRouting
 import com.systems.pontini.config.configureStatusPages
 import com.systems.pontini.core.security.configureSecurity
-import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.server.application.Application
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+private const val HOST = "0.0.0.0"
+private const val PORT = 8080
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = PORT, host = HOST , module = Application::module)
         .start(wait = true)
 }
 
