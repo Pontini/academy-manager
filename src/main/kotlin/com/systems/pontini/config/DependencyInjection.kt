70d38c7c3d70127aa87c5b8966850baa2e7a65d2
ewerton-pontini <ewerton.pontini@picpay.com>
2023-05-01 14:00:19 -0300

    refactor code

--- src/main/kotlin/com/systems/pontini/config/DependencyInjection.kt
+++ src/main/kotlin/com/systems/pontini/config/DependencyInjection.kt
@@ -1,20 +1,7 @@
 package com.systems.pontini.config
 
-import com.systems.pontini.core.Mapper
-import com.systems.pontini.features.auth.data.datasource.mapper.ResultRowToUserMapper
-import com.systems.pontini.features.auth.data.repository.AuthRepositoryImpl
-import com.systems.pontini.features.auth.data.service.auth.AuthDataSource
-import com.systems.pontini.features.auth.data.service.auth.AuthLocalDataSourceImpl
-import com.systems.pontini.features.auth.domain.model.User
-import com.systems.pontini.features.auth.domain.repository.AuthRepository
-import com.systems.pontini.features.auth.domain.usecase.LoginUserUseCase
-import com.systems.pontini.features.auth.domain.usecase.LoginUserUseCaseImpl
-import com.systems.pontini.features.auth.domain.usecase.RegisterUserUseCase
-import com.systems.pontini.features.auth.domain.usecase.RegisterUserUseCaseImpl
-import com.systems.pontini.features.auth.presentation.AuthController
+import com.systems.pontini.features.auth.di.authModule
 import io.ktor.server.application.*
-import org.jetbrains.exposed.sql.ResultRow
-import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
@@ -23,15 +10,5 @@ fun Application.configureDependencyInjection() {
         slf4jLogger()
         modules(authModule)
     }
-
-}
-
-val authModule = module {
-    single { AuthController(get(),get()) }
-    single<RegisterUserUseCase> { RegisterUserUseCaseImpl(get(),) }
-    single<LoginUserUseCase> { LoginUserUseCaseImpl(get()) }
-    single<AuthRepository> { AuthRepositoryImpl(get()) }
-    single<AuthDataSource> { AuthLocalDataSourceImpl(get()) }
-    single<ResultRowToUserMapper> { ResultRowToUserMapper() }
 
 }
